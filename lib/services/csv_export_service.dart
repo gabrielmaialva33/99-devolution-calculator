c68996a92e9f672aebdea5e26dcb41b5387051f6
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-09-02 14:50:30 -0300

    feat: Add permission_handler dependency for CSV export
    This commit adds the `permission_handler` package as a dependency.
    
    This change is preparatory for implementing CSV export functionality which will require storage permissions. The `permission_handler` will be used to request necessary permissions from the user before attempting to save or share files.

--- lib/services/csv_export_service.dart
+++ lib/services/csv_export_service.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'package:csv/csv.dart';
 import 'package:intl/intl.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:share_plus/share_plus.dart';
 import '../features/scanner/domain/models/barcode_item.dart';
 
